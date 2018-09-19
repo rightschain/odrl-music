aaf5e2dc7562a86524beb87fa15bc926852d91d1
Renato Iannella <r@iannel.la>
2018-09-19 17:36:14 +1000

    1st draft

---
+++ config.js
@@ -0,0 +1,116 @@
+var respecConfig = {
+    specStatus: "ED",
+    shortName: "odrl-music",
+    edDraftURI: "https://rightschain.github.io/odrl-music/",
+    editors: [ {   name:       "Renato Iannella",
+                    url:        "https://au.linkedin.com/in/riannella",
+                    company:    "RightsChain",
+                    companyURL: "http://www.rightschain.info/",
+                    mailto:     "ri@rightschain.info"
+                }
+             ],
+    wg: "RightsChain",
+    logos: [
+        { src:  "http://files.gandi.ws/gandi91666/image/logo.png",
+            href: "http://www.rightschain.info/",
+            alt: "Rights Chain",
+            width: 100 }
+    ],
+    wgURI: "http://www.rightschain.info/",
+    wgPublicList: "",
+    inlineCSS: true,
+    noIDLIn: true,
+    noLegacyStyle: false,
+    noRecTrack: true,
+    overrideCopyright: "<b>Copyright:</b> 2018 RightsChain. All Rights Reserved.",
+    localBiblio:  {
+        "ISO15707": {
+            title: "International Standard musical Work Code (ISWC)",
+            publisher: "ISO",
+            status: "ISO 15707:2001(E)",
+            date: "2001",
+            href: "http://www.iso.org/iso/catalogue_detail?csnumber=28780"
+        },
+        "ISO3901": {
+            title: "International Standard Recording Code (ISRC)",
+            publisher: "ISO",
+            status: "ISO 3901:2001(E)",
+            date: "2001",
+            href: "http://www.iso.org/iso/catalogue_detail?csnumber=23401"
+        },
+        "ISO27729": {
+            title: "International Standard Name Identifier (ISNI)",
+            publisher: "ISO",
+            status: "ISO 27729:2012(E)",
+            date: "2012",
+            href: "http://www.iso.org/iso/catalogue_detail?csnumber=44292"
+        },
+        "RECORD-CREDIT": {
+            title: "Credits and Recording Metadata",
+            publisher: "Recording Academy",
+            status: "Producers & Engineers Wing",
+            href: "https://www.grammy.org/files/pages/basic_credits_metadata_list.pdf"
+        },
+        "DDEX-MM": {
+            title: "Musical Work Licensing Message Suite Standard",
+            publisher: "Digtial Data Exchange (DDEX)",
+            status: "Version 2.1 Draft Standard",
+            date: "20 July 2012",
+            href: "https://kb.ddex.net/display/HBK/Musical+Works+Licensing+Message+Suite+Standard"
+        },
+        "CISAC-CWR": {
+            title: "Functional specifications: Common Works Registration",
+            publisher: "International Confederation of Societies of Authors and Composers (CISAC)",
+            status: "Version 2.2",
+            date: "14 April 2016",
+            href: "http://members.cisac.org/CisacPortal/initConsultDoc.do?id=29541"
+        },
+        "CISAC-TABLE": {
+            title: "AVIndex and AVR Lookup Tables",
+            publisher: "International Confederation of Societies of Authors and Composers (CISAC)",
+            status: "Version AVI08-2245R3",
+            date: "28 October 2011",
+            href: "http://members.cisac.org/CisacPortal/cisacDownloadFile.do?docId=22243"
+        },
+        "GETTY-TGN": {
+            title: "Getty Thesaurus of Geographic Names",
+            publisher: "The J. Paul Getty Trust",
+            date: "2015",
+            href: "http://www.getty.edu/research/tools/vocabularies/tgn/"
+        },
+        "IFLA-FRBR": {
+            title: "Functional Requirements for Bibliographic Records",
+            publisher: "International Federation of Library Associations",
+            date: "Feb 2009",
+            href: "http://www.ifla.org/publications/functional-requirements-for-bibliographic-records/"
+        },
+        "FIBO": {
+            title: "Financial Industry Business Ontology",
+            publisher: "EDMC/OMG",
+            date: "2016",
+            href: "http://www.omg.org/spec/EDMC-FIBO/"
+        },
+        "IFPI-GRID": {
+            title: "Global Release Identifier (GRid) Standard",
+            publisher: "IFPI",
+            status: "Version 2.1",
+            date: "2007-02-26",
+            href: "http://www.ifpi.org/downloads/GRid_Standard_v2_1.pdf"
+        },
+        "MBA-STYLE": {
+            title: "Music Metadata Style Guide",
+            publisher: "Music Business Association",
+            status: "Version 2.1",
+            date: "2015-09-29",
+            href: "https://musicbiz.org/wp-content/uploads/2016/04/MusicMetadataStyleGuide_V2.1.pdf"
+        },
+        "W3C-CLAIM": {
+            title: "Verifiable Claims Data Model and Representations",
+            publisher: "W3C Credentials Community Group",
+            status: "Draft",
+            date: "2016-12-15",
+            href: "https://opencreds.github.io/vc-data-model/"
+        }
+    },
+    lint: false
+};
